59a2e27b0574ad94cfff037d051a00506381d0c7
Mohit Kanwal <mohit.kanwal@gmail.com>
2013-01-31 01:20:06 +0800

    Files for App Uploads

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import panex_web.config
+import re
 
 # Create your models here.
+
+class App(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    location = models.CharField(max_length=400)
+    version = models.CharField(max_length=10, default="1.0.0")
+    location = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    downloads = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name + "\n" + self.description
+
+    def is_updated(self, inputVersion):
+    	if version_checker(self.version, inputVersion) > 0:
+    		return True
+    	return False
+
+    def version_checker(version1, version2):
+    	def normalize(v):
+    		return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+    	return cmp(normalize(version1), normalize(version2))
+
+# 
+# Small test suite to check the version checker function
+# 
+if __name__ == '__main__':
+	assert mycmp('1.1.1.1', '1.1.1.1') == 0
+	assert mycmp('1.1.1.2', '1.1.1.1') == 1
--- panex_web/config.py
+++ panex_web/config.py
@@ -1,2 +1,13 @@
-# import config
+# 
+# Use import config
 # All directory locations will be stored here
+# Don't forget the trailing slashes
+# 
+# 
+# Default directory to store apps
+APP_DIRECTORY = '/tmp/app/'
+
+# Default directory to store services
+SERVICE_DIRECTORY = '/tmp/service/'
+
+# define utility functions
